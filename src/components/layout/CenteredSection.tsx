67e14f379632a7cc1c1cc8b14e451d2673d52027
diogobonet <diogosobezak@gmail.com>
2025-08-18 19:56:52 -0300

    feat: adiciona componente de secao centralizada e pagina de selecionar organizacao
    Signed-off-by: diogobonet <diogosobezak@gmail.com>

--- src/components/layout/CenteredSection.tsx
+++ src/components/layout/CenteredSection.tsx
@@ -6,8 +6,10 @@ interface CenteredSectionProps {
 
 export default function CenteredSection({ children }: CenteredSectionProps) {
   return (
-    <div className="centered-section">
-      {children}
+    <div className="centered-container">
+      <div className="centered-section">
+        {children}
+      </div>
     </div>
   );
 }
--- src/pages/organization/SelectOrganization.tsx
+++ src/pages/organization/SelectOrganization.tsx
@@ -1,15 +1,33 @@
-// import React from 'react'
+import { Button } from '@mui/material';
 import CenteredSection from "../../components/layout/CenteredSection"
 import SimpleHeader from "../../components/layout/SimpleHeader"
+import { useNavigate } from 'react-router-dom';
 
 export default function SelectOrganization() {
+  const navigate = useNavigate();
+
   return (
     <div>
         <SimpleHeader></SimpleHeader>
         <main>
             <CenteredSection>
-                <h1>Selecione a organização</h1>
-                <p>Selecione a sua organização!</p>
+                <div className="organization-section">
+                  <div className="organization-title">
+                    <h1>Selecione a organização</h1>
+                    <p>Selecione a sua organização!</p>
+                  </div>
+
+                  <div className="project-organization-select">
+                    <div className="project-organization-infos">
+                      <label htmlFor="">Organization Name</label>
+                      <p>Description about my organization</p>
+                    </div>
+
+                    <div>
+                        <Button className="primary-button" variant="contained" onClick={() => navigate("/projects")}>Entrar</Button>
+                    </div>
+                  </div>
+                </div>
             </CenteredSection>
         </main>
     </div>
