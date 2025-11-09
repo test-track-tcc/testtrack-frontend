3d55ac231db2cd263a4e29990cc6db0e01a2188b
diogobonet <diogosobezak@gmail.com>
2025-11-08 21:09:17 -0300

    fix: ajusta paginas para deploy
    Signed-off-by: diogobonet <diogosobezak@gmail.com>

--- src/pages/scripts/ScriptsPage.tsx
+++ src/pages/scripts/ScriptsPage.tsx
@@ -27,8 +27,6 @@ import { type Project } from '../../types/Project';
 import { ProjectService } from '../../services/ProjectService';
 import { useNavigate } from 'react-router-dom';
 
-const API_URL = import.meta.env.VITE_API_BASE_URL;
-
 export default function ScriptsPage() {
     const { orgId, projectId } = useParams<{
         orgId: string;
